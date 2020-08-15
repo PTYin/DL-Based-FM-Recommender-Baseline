9117be1dbfec1d3468b4e057a34558c8ee3d8104
ptyin <peteryin1604@gmail.com>
2020-08-15 14:49:29 +0800

    rm app.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import argparse
+import yaml
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
@@ -151,3 +153,18 @@ def run(config):
         if 'tag' in config:
             torch.save(model, os.path.join(config['model']['model_path'],
                                            '{}_{}.pth'.format(config['model']['name'], config['tag'])))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config',
+                        type=str,
+                        default='../config_example/DeepFM_load.yaml',
+                        help='path for configure file')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config):
+        print("config file doesn't exist")
+        exit(-1)
+    run(yaml.load(open(args.config, 'r').read(), Loader=yaml.FullLoader))
+
