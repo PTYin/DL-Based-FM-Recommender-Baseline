942b2c3693b67192ce21bf81ce1e0c60ec520168
ptyin <peteryin1604@gmail.com>
2020-08-18 00:37:15 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd src/
+embedding_size = $1
+datasets = ("automotive", "books", "clothing", "ml-1m", "office", "ticket")
+models = ("DeepFM", "XDeepFM")
+config_dir = "../config/rating"
+
+for dataset in ${datasets} do
+  for model in ${models} do
+    if [ ! -d "/home/share/yinxiangkun/log/libFM/${embedding_size}/${dataset}/" ]; then
+      mkdir "/home/share/yinxiangkun/log/libFM/${embedding_size}/${dataset}/"
+    fi
+    echo "${config_dir}/${embedding_size}/${dataset}/${model}"
+#    python app.py --config "${config_dir}/${embedding_size}/${dataset}/${model}" > "/home/share/yinxiangkun/log/libFM/${embedding_size}/${dataset}/${model}.txt"
+  done
+done
